809dce9dcba1434be95176ec11fca7e3b5aebc19
IMALKAPERERA\imalk <imalkaperera07@gmail.com>
2024-07-02 04:13:10 +0530

    learned about use of const

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Fname = "Imalka";
+const Lname = "Perera";
+const number = "07";
+const creat = "Imalka Perera";
+
+
+const CurrentDate = new Date();                   {/*the current date will show in the cosole area(the website inspect area)*/}
+const year = CurrentDate.getFullYear;           {/*this shows the current year in the cosole area*/}
+console.log (year);
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h1>Hello {`${Fname} ${Lname}`}!</h1>               {/* u can use {Fname} {Lname}as well.*/}            
+       <p>My lucky number is {number}.</p>                  {/*u can use {3 + 4 }as well. */} 
+       <p>Created By {creat}</p>
+       <p>Copyright {year}</p>
+              
+            
+
+    </div>        
   );
 }
 
